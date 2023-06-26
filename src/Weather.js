8a8bafe2f224f39c47f027942c5234a560e8d157
tamarajakimovska <tamarajakimovska007@gmail.com>
2023-06-26 21:16:24 +0200

    Added axios

--- src/Weather.js
+++ src/Weather.js
@@ -9,5 +9,5 @@ export default function Weather(props) {
   let apiKey = "3a94f3778290bfeee61278505dbbe51d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather</h2>;
+  return <h2>Hi from Weather</h2>;
 }
